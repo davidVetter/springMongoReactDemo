52512a1e1aa3240d4ac8eb531ece086aaa95680d
David Vetter <ddvetter23@gmail.com>
2023-02-05 16:43:02 -0600

    initial commit, brought in fresh gradle project

---
+++ src/test/java/com/vetterdev/springProject/springMongoProject/SpringMongoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetterdev.springProject.springMongoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
